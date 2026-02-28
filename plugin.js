9fe28a795de8ba224931299126fe951ff1d75026
rheno felipe <92105197+rheno911@users.noreply.github.com>
2026-02-28 03:45:19 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,25 +1,59 @@
-function getManifest(){
-    return {name:"LaMovie",id:"com.rheno911.lamovie",version:1,baseUrl:"https://la.movie"};
+function getManifest() {
+  return {
+    "name": "LaMovie",
+    "id": "com.rheno911.lamovie", 
+    "version": 1,
+    "baseUrl": "https://la.movie"
+  };
 }
 
-function getHome(callback){
-    callback({type:"success",value:'{"Featured":[{"title":"Test Movie","url":"61323","posterUrl":"https://via.placeholder.com/300x450"}]}'});
+function getHome(callback) {
+  callback({
+    "type": "success",
+    "value": JSON.stringify({
+      "Featured": [{
+        "title": "Test Movie",
+        "url": "61323",
+        "posterUrl": "https://via.placeholder.com/300x450?text=LaMovie"
+      }]
+    })
+  });
 }
 
-function search(query,callback){
-    callback({type:"success",value:'[{"title":"'+query+'","url":"61323","posterUrl":"https://via.placeholder.com/300x450"}]'});
+function search(query, callback) {
+  callback({
+    "type": "success", 
+    "value": JSON.stringify([{
+      "title": query + " (Test)",
+      "url": "61323",
+      "posterUrl": "https://via.placeholder.com/300x450?text=LaMovie"
+    }])
+  });
 }
 
-function load(id,callback){
-    callback({type:"success",value:JSON.stringify({
-        url:id,data:id,title:"Test Movie (2010)",description:"Working!",year:2010
-    })});
+function load(id, callback) {
+  callback({
+    "type": "success",
+    "value": JSON.stringify({
+      "url": id,
+      "data": id,
+      "title": "Una loca película de vampiros (2010)",
+      "description": "Test stream working!",
+      "year": 2010
+    })
+  });
 }
 
-function loadStreams(id,callback){
-    callback({type:"success",value:JSON.stringify([{
-        name:"Auto - LaMovie",
-        url:"https://s15.vimeos.zip/hls2/01/00009/8l2e7h1uapnz_h/master.m3u8",
-        headers:{"User-Agent":"Mozilla/5.0","Referer":"https://player.vimeo.com/"}
-    }])});
+function loadStreams(id, callback) {
+  callback({
+    "type": "success",
+    "value": JSON.stringify([{
+      "name": "HD - LaMovie",
+      "url": "https://s15.vimeos.zip/hls2/01/00009/8l2e7h1uapnz_h/master.m3u8",
+      "headers": {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+        "Referer": "https://player.vimeo.com/"
+      }
+    }])
+  });
 }
