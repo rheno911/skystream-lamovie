34cca034b821c07113742a59ba514fb581a5c826
rheno felipe <92105197+rheno911@users.noreply.github.com>
2026-02-28 20:53:42 -0600

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,5 +1,5 @@
-function getManifest(){return{name:"LaMovie",id:"com.rheno911.lamovie.debug",version:1,baseUrl:"https://la.movie"};}
-function getHome(callback){callback({type:"success",value:'{"Test":[{"title":"Working!","url":"61323","posterUrl":""}]'});}
-function search(query,callback){callback({type:"success",value:'[{"title":"'+query+'","url":"61323","posterUrl":""}]'});}
-function load(id,callback){callback({type:"success",value:'{"url":"'+id+'","data":"'+id+'","title":"Movie"}'});}
-function loadStreams(id,callback){callback({type:"success",value:'[{"name":"HD","url":"https://s15.vimeos.zip/hls2/01/00009/8l2e7h1uapnz_h/master.m3u8","headers":{"Referer":"https://player.vimeo.com/"}}]'});}
+function getManifest(){return{name:"LaMovie",id:"com.rheno911.lamovie",version:9};}
+function getHome(callback){callback({type:"success",value:'{"Peliculas":[{"title":"Test LaMovie","url":"61323","posterUrl":"https://la.movie/wp-content/uploads"}]'});}
+function search(query,callback){callback({type:"success",value:'[{"title":"'+query+' LaMovie","url":"61323","posterUrl":"https://la.movie/wp-content/uploads"}]'});}
+function load(id,callback){callback({type:"success",value:'{"url":"'+id+'","data":"'+id+'","title":"Pelicula ID '+id+'","year":2024}'});}
+function loadStreams(id,callback){callback({type:"success",value:'[{"name":"HD LaMovie","url":"https://s15.vimeos.zip/hls2/01/00009/8l2e7h1uapnz_h/master.m3u8","headers":{"Referer":"https://la.movie/"}}]'});}
