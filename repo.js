f12af0f34973a91786ae6d4dd2451b2d578ac058
rheno felipe <92105197+rheno911@users.noreply.github.com>
2026-02-28 21:32:51 -0600

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,12 @@
+{
+  "plugins": [
+    {
+      "name": "LaMovie",
+      "internalName": "LaMovie",
+      "url": "https://raw.githubusercontent.com/rheno911/skystream-lamovie/main/LaMovie.sky",
+      "version": 1,
+      "icon": "https://via.placeholder.com/128?text=LM",
+      "description": "LaMovie movies & series"
+    }
+  ]
+}
